992f7a495e36f540872f074f5cf9fee53a40f08a
sheamcdevitt <52835855+sheamcdevitt@users.noreply.github.com>
2022-02-08 21:32:28 +0000

    added untracked files

---
+++ src/containers/about/import.js
@@ -0,0 +1,37 @@
+import twitter from "../../assets/twitter-purple.png";
+import linkedin from "../../assets/linkedin-purple.png";
+import computer from "../../assets/computer.png";
+import github from "../../assets/github-purple.png";
+import kx from "../../assets/kx.png";
+import html from "../../assets/html.png";
+import js from "../../assets/javascript.png";
+import css from "../../assets/css.png";
+import linux from "../../assets/linux.png";
+import react from "../../assets/react.png";
+import csharp from "../../assets/csharp.png";
+import cplusplus from "../../assets/c++.png";
+import sql from "../../assets/mysql.png";
+import java from "../../assets/java.png";
+import python from "../../assets/python.png";
+import opencv from "../../assets/opencv.png";
+import d3 from "../../assets/d3.png";
+
+export {
+  linkedin,
+  twitter,
+  computer,
+  github,
+  kx,
+  html,
+  js,
+  css,
+  linux,
+  react,
+  csharp,
+  cplusplus,
+  sql,
+  java,
+  python,
+  opencv,
+  d3,
+};
